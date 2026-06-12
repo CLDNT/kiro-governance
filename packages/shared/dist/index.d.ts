0e5ccce71bbcb71b5ad2c4d9592636aa8cf38271
Muhammad Faraz <faraxshams@gmail.com>
2026-06-12 18:53:40 +0500

    Dist removed

--- packages/shared/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { GovernanceEventRecord, DeduplicatedSentinelRecord } from './types/governance-event';
-export { MACRO_GATES, MACRO_GATE_ALIASES, classifyEvent } from './constants/macro-gates';
-export type { MacroGate } from './constants/macro-gates';
